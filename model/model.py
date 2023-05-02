52b175a7ba43c2c0db0076d2360fd1a7959d8f41
Tatyana Sgibneva <sgibnevatata@gmail.com>
2023-05-03 00:04:52 +0300

    первый рефакторинг готов

---
+++ model/model.py
@@ -0,0 +1,52 @@
+import os
+
+from selene import browser, have
+
+
+class RegistrationForm:
+
+    def open(self):
+        browser.open('/automation-practice-form')
+
+    def user_first_name_fill(self, value):
+        browser.element('#firstName').type(value)
+
+    def user_last_name_fill(self, value):
+        browser.element('#lastName').type(value)
+
+    def user_email_fill(self, value):
+        browser.element('#userEmail').type(value)
+
+    def user_gender_select(self, value):
+        browser.all('[name=gender]').element_by(have.value(value)).element('..').click()
+
+    def user_phone_fill(self, value):
+        browser.element('#userNumber').type(value)
+
+    def user_birtday(self, day, month, year):
+        browser.element('#dateOfBirthInput').click()
+        browser.element(f'.react-datepicker__year-select [value="{year}"]').click()
+        browser.element(f'.react-datepicker__month-select [value="{month}"]').click()
+        browser.element(f'.react-datepicker__day.react-datepicker__day--0{day}.react-datepicker__day').click()
+
+    def user_subject_fill(self, value1, value2):
+        browser.element('#subjectsInput').type(value1).press_enter().type(value2).press_enter()
+
+    def user_hobbies_select(self, value):
+        browser.element(f'[for = hobbies-checkbox-{value}]').click()
+
+    def file_select(self, file_name):
+        browser.element('#uploadPicture').send_keys(os.getcwd() + file_name)
+
+    def user_address_fill(self, value):
+        browser.element('#currentAddress').type(value)
+
+    def user_state_and_sity_select(self, state_value, sity_value):
+        browser.element('[id=react-select-3-input]').type(state_value).press_enter()
+        browser.element('[id=react-select-4-input]').type(sity_value).press_enter()
+
+    def form_submit(self):
+        browser.element('[id=submit]').press_enter()
+
+    def result_table(self):
+        return browser.element('.table-responsive').all('td')
--- tests/test_automation_practice_form.py
+++ tests/test_automation_practice_form.py
@@ -1,38 +1,9 @@
-import pytest
-from selene import browser, have
-import os
+from selene import have
+from model.model import RegistrationForm
 
-class RegistrationForm:
-
-    def open(self):
-        browser.open('/automation-practice-form')
-
-    def user_first_name_fill(self, value):
-        browser.element('#firstName').type('value')
-
-    def user_last_name_fill(self, value):
-        browser.element('#lastName').type('value')
-
-    def user_email_fill(self, value):
-        browser.element('#userEmail').type(value)
-
-    def user_gender_select(self, value):
-        browser.all('[name=gender]').element_by(have.value(value)).element('..').click()
-
-    def user_phone_fill(self, value):
-        browser.element('#userNumber').type(value)
-
-    def user_birtday(self, day, month, year):
-        browser.element('#dateOfBirthInput').click()
-        browser.element(f'.react-datepicker__year-select [value="{year}"]').click()
-        browser.element(f'.react-datepicker__month-select [value="{month}"]').click()
-        browser.element(f'.react-datepicker__day.react-datepicker__day--0{day}.react-datepicker__day').click()
-
-    def user_subject_fill(self, value1, value2):
-        browser.element('#subjectsInput').type(value1).press_enter().type(value2).press_enter()
 
 def test_success_form_send(browser_settings):
-    registration_form  = RegistrationForm()
+    registration_form = RegistrationForm()
     registration_form.open()
 
     registration_form.user_first_name_fill('tata')
@@ -40,27 +11,22 @@ def test_success_form_send(browser_settings):
     registration_form.user_email_fill('test@test.ru')
     registration_form.user_gender_select('Female')
     registration_form.user_phone_fill('79999999999')
-    registration_form.user_birtday('15','5','1985')
+    registration_form.user_birtday('15', '5', '1985')
     registration_form.user_subject_fill('co', 'ma')
+    registration_form.user_hobbies_select('2')
+    registration_form.file_select('/bradford.jpg')
+    registration_form.user_address_fill('тестовый адрес')
+    registration_form.user_state_and_sity_select('NCR', 'Delhi')
+    registration_form.form_submit()
 
+    registration_form.result_table().should(have.texts('Student Name', 'tata test',
+                                                       'Student Email', 'test@test.ru',
+                                                       'Gender', 'Female',
+                                                       'Mobile', '7999999999',
+                                                       'Date of Birth', '15 June,1985',
+                                                       'Subjects', 'Computer Science, Maths',
+                                                       'Hobbies', 'Reading',
+                                                       'Picture', 'bradford.jpg',
+                                                       'Address', 'тестовый адрес',
+                                                       'State and City', 'NCR Delhi'))
 
-
-    browser.element('[for = hobbies-checkbox-2]').click()
-    browser.element('#uploadPicture').send_keys(os.getcwd() + '/bradford.jpg')
-    browser.element('#currentAddress').type('тестовый адрес')
-
-    browser.element('[id=react-select-3-input]').type('NCR').press_enter()
-    browser.element('[id=react-select-4-input]').type('Delhi').press_enter()
-    browser.element('[id=submit]').press_enter()
-    ...
-    browser.element('.table-responsive').all('td').should(have.texts('Student Name', 'tata test',
-                                                                     'Student Email', 'test@test.ru',
-                                                                     'Gender', 'Female',
-                                                                     'Mobile', '7999999999',
-                                                                     'Date of Birth', '15 May,1985',
-                                                                     'Subjects', 'Computer Science, Maths',
-                                                                     'Hobbies', 'Reading',
-                                                                     'Picture', 'bradford.jpg',
-                                                                     'Address', 'тестовый адрес',
-                                                                     'State and City', 'NCR Delhi'))
-    ...
