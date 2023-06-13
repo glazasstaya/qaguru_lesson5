5a4328844f49acf7e3218213829c4743592685ce
Tatyana Sgibneva <sgibnevatata@gmail.com>
2023-06-13 09:59:55 +0300

    Добавлены allure.step

--- tests/test_automation_practice_form.py
+++ tests/test_automation_practice_form.py
@@ -1,8 +1,9 @@
+import allure
 from selene import have
 from page_objects.registration_page_object import RegistrationForm
 from data_objects.registrathion_page_data_object import Gender, Month, UserData
 
-
+@allure.title('Success fill form')
 def test_success_form_send(browser_settings):
 
     registration_form = RegistrationForm()
@@ -23,17 +24,23 @@ def test_success_form_send(browser_settings):
                     sity = 'Delhi'
                     )
 
-    registration_form.open()
-    registration_form.form_fill(user)
-    registration_form.form_submit()
+    with allure.step('Open form'):
+        registration_form.open()
 
-    registration_form.result_table().should(have.texts('Student Name', 'tata test',
-                                                     'Student Email', 'test@test.ru',
-                                                     'Gender', 'Female',
-                                                     'Mobile', '7999999999',
-                                                     'Date of Birth', '11 June,1985',
-                                                     'Subjects', 'Computer Science, Maths',
-                                                     'Hobbies', 'Reading',
-                                                     'Picture', 'bradford.jpg',
-                                                     'Address', 'тестовый адрес',
-                                                     'State and City', 'NCR Delhi'))
+    with allure.step('Fill form'):
+        registration_form.form_fill(user)
+
+    with allure.step('Submit form'):
+        registration_form.form_submit()
+
+    with allure.step('Check result'):
+        registration_form.result_table().should(have.texts('Student Name', 'tata test',
+                                                         'Student Email', 'test@test.ru',
+                                                         'Gender', 'Female',
+                                                         'Mobile', '7999999999',
+                                                         'Date of Birth', '11 June,1985',
+                                                         'Subjects', 'Computer Science, Maths',
+                                                         'Hobbies', 'Reading',
+                                                         'Picture', 'bradford.jpg',
+                                                         'Address', 'тестовый адрес',
+                                                         'State and City', 'NCR Delhi'))
